cadf6b2eebbceb6bca538e2a9d4f0c18643f476f
andy910829 <71509005+andy910829@users.noreply.github.com>
2022-09-06 22:18:34 +0800

    Add files via upload

--- internet_check_linebot/app.py
+++ internet_check_linebot/app.py
@@ -1,7 +1,7 @@
 from re import search
 import time
 from flask import Flask, request, abort
-
+from flask_ngrok import run_with_ngrok
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -9,32 +9,96 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-from internet_check import main2
+from internet_check import internet_check
+import json
+import threading
 
 error_area = []
 
 app = Flask(__name__)
 
-# Channel Access Token
+#Channel Access Token
 line_bot_api = LineBotApi('QIsD3hg2VgTge6iTh/bSfUjUOgMdPlBCPXlTtG/5bOfeq2PqAVufvH192wO/XgxlrmRV7Vnrk630vS9EbYq+XnyHoCzNFD+VH67TgIT77eCTylU28zpbo/f++TVrL5QNP5jBo4225ZREsD2eeZFxXgdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('7725e2081253d481db9edd9273be30f0')
 
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
+checker = internet_check()
+
+@app.route("/", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
     # get request body as text
     body = request.get_data(as_text=True)
+
     app.logger.info("Request body: " + body)
+
     # handle webhook body
     try:
+        # json_data = json.loads(body) 
         handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+        # msg = json_data['events'][0]['message']['text']      # 取得 LINE 收到的文字訊息
+        # tk = json_data['events'][0]['replyToken'] 
+        # line_bot_api.reply_message(tk,TextSendMessage(msg))  # 回傳訊息
+        # print(msg, tk)  
+    except:
+        print(body) 
     return 'OK'
 
+
+@handler.add(MessageEvent, message=TextMessage)     #使用者傳訊息會觸發
+def handle_message(event):
+    if event.message.text != '':
+        # line_bot_api.reply_message(event.reply_token, 
+        #     TextSendMessage(text='掃描中...'))
+        line_bot_api.reply_message(event.reply_token, 
+            TextSendMessage(text=checker.ans))
+
+
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target = checker.store)   #開始更新checker裡面ans
+    t1.start()
+    run_with_ngrok(app)                             #run_with_ngrok 產生webhook
+    app.run()                                       #無限迴圈
+
+
+# @app.route("/", methods=['POST'])
+# def linebot():
+#     body = request.get_data(as_text=True)                    # 取得收到的訊息內容
+#     try:
+#         json_data = json.loads(body)                         # json 格式化訊息內容
+#         access_token =  'QIsD3hg2VgTge6iTh/bSfUjUOgMdPlBCPXlTtG/5bOfeq2PqAVufvH192wO/XgxlrmRV7Vnrk630vS9EbYq+XnyHoCzNFD+VH67TgIT77eCTylU28zpbo/f++TVrL5QNP5jBo4225ZREsD2eeZFxXgdB04t89/1O/w1cDnyilFU='
+#         secret = '7725e2081253d481db9edd9273be30f0'
+#         line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
+#         handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+#         signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
+#         handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+#         msg = json_data['events'][0]['message']['text']      # 取得 LINE 收到的文字訊息
+#         tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
+#         line_bot_api.reply_message(tk,TextSendMessage(msg))  # 回傳訊息
+#         print(msg, tk)                                       # 印出內容
+#     except:
+#         print(body)                                          # 如果發生錯誤，印出收到的內容
+#     return 'OK'    
+# def linebot(request):
+#     try:
+#         access_token = 'QIsD3hg2VgTge6iTh/bSfUjUOgMdPlBCPXlTtG/5bOfeq2PqAVufvH192wO/XgxlrmRV7Vnrk630vS9EbYq+XnyHoCzNFD+VH67TgIT77eCTylU28zpbo/f++TVrL5QNP5jBo4225ZREsD2eeZFxXgdB04t89/1O/w1cDnyilFU='
+#         secret = '7725e2081253d481db9edd9273be30f0'
+#         body = request.get_data(as_text=True)
+#         json_data = json.loads(body)
+#         line_bot_api = LineBotApi(access_token)
+#         handler = WebhookHandler(secret)
+#         signature = request.headers['X-Line-Signature']
+#         handler.handle(body, signature)
+#         msg = json_data['events'][0]['message']['text']
+#         tk = json_data['events'][0]['replyToken']
+#         line_bot_api.reply_message(tk,TextSendMessage(msg))
+#         print(msg, tk)
+#     except:
+#         print(request.args)
+#     return 'OK'
 
 
 # def push_warning():
@@ -43,30 +107,33 @@ def callback():
 #     user_id =  'U33dcbe139d669eb71298956ab6e24a08'
 #     line_bot_api.push_message(user_id, 
 #             TextSendMessage(text="123"))
-    # for i in range(197):
-    #     ip,name,status,ping = main2(i)
-    #     if ip != 'None':
-    #         ans  = ans  + name + '--目前網路無法使用'+ '\n'
-    # line_bot_api.push_message('<to>', 
-    #         TextSendMessage(text=ans))
-    # print('done')
-    
-                        
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    if event.message.text == 'start':
-        user_id =  event.source.user_id
-        line_bot_api.push_message(user_id, 
-                TextSendMessage(text="123"))
-        print(123)
-        ans = main2()
-        line_bot_api.push_message(user_id, 
-            TextSendMessage(text=ans))
-        print('done')
+#     for i in range(197):
+#         ip,name,status,ping = main2(i)
+#         if ip != 'None':
+#             ans  = ans  + name + '--目前網路無法使用'+ '\n'
+#     line_bot_api.push_message('<to>', 
+#             TextSendMessage(text=ans))
+#     print('done')
 
 
-
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+# while True:
+        #     line_bot_api.push_message(user_id, 
+        #                 TextSendMessage(text="123"))
+        #     print('123')
+        #     await wait_for(line_bot_api.push_message(  # 回復傳入的訊息文字
+        #                     user_id,
+        #                     TemplateSendMessage(
+        #                         alt_text='Buttons template',
+        #                         template=ButtonsTemplate(
+        #                             title='學校網路測試',
+        #                             text='點下面按鈕開始測試',
+        #                             actions=[
+        #                                 MessageTemplateAction(
+        #                                     label='開始測試',
+        #                                     text='開始測試'
+        #                                 )
+        #                             ]
+        #                         )
+        #                     )
+        #                 ))
+        #if event.message.text == 'test':
--- internet_check_linebot/internet_check.py
+++ internet_check_linebot/internet_check.py
@@ -1,56 +1,93 @@
 import requests
 import json
+import time
 
 with open('urllist.json') as f:
     data = json.load(f)
+
 
 localhost = []
 area = []
 encrypt_host = []
     
+class internet_check:
+    def __init__(self):#初始化
+        self.ans = ' ' 
+        self.localhost = []
+        self.area = []
+        self.encrypt_host = []
+        self.ip=' '
+        self.name=' '
+        self.status=' '
+        self.ping =' '
+        with open('urllist.json') as f:
+            self.data = json.load(f)
+        
+    def datatoarray(self):                                              #轉換類型
+        for i in range(len(self.data)):
+            self.localhost.append(self.data[i]['host'])
+            self.area.append(self.data[i]['name'])
+            self.encrypt_host.append(self.data[i]['encrypt_host'])
 
-def datatoarray(data):
-    global localhost,area,encrypt_host
-    for i in range(len(data)):
-        localhost.append(data[i]['host'])
-        area.append(data[i]['name'])
-        encrypt_host.append(data[i]['encrypt_host'])
-    
-def connection_check(i):
-    try:
-        global localhost,area,encrypt_host
-        r = requests.get(f"https://netflow.ntut.edu.tw/home/network/ping?encrypt_host={encrypt_host[i]}")
-        check = r.text
-        #print(localhost[i], area[i], r.status_code, r.text)
-        if check == 'false':
-            return localhost[i], area[i], r.status_code, r.text
-        else:
-            return 'None','None','None','None'
-    except TimeoutError:
-        return localhost[i], area[i], r.status_code, r.text
+    def connection_check(self):                                            #判斷連線成不成功 #
+        try:
+            for i in range(197):
+                headers = {
+                'user-agent' : 'Mozilla/5.0'
+                }
+                r = requests.get(f"https://netflow.ntut.edu.tw/home/network/ping?encrypt_host={self.encrypt_host[i]}", headers = headers )
+                check = r.text
+                # print(self.localhost[i], self.area[i], r.status_code, r.text)
+                if check == 'false':   
+                    self.ip = self.localhost[i]    
+                    self.name = self.area[i]
+                    self.status =   r.status_code    
+                    self.ping = r.text                          #連線失敗
+                    # return localhost[i], area[i], r.status_code, r.text
+                    self.ans= self.ans+ self.area[i] + '--目前網路無法使用'+ '\n'
+            # else:                                                       #連線正常
+            #     return 'None','None','None','None'
+            print('done')
+        except TimeoutError:                                            #連線失敗
+                # return localhost[i], area[i], r.status_code, r.text
+                self.ans= self.ans+ area[i] + '--目前網路無法使用'+ '\n'
+
+    def run(self): #轉類+確認+給出答覆
+        self.datatoarray()
+        self.connection_check()
+    def store(self):#
+        while True:
+            self.run()
+            time.sleep(300)
 
 
-def main():
-    datatoarray(data)
-    global localhost
-    for i in range(197):
-        ip,name,status,ping = connection_check(i)
-        if ip != 'None':
-            print(ip,name,status,ping)
+
+# if __name__ == '__main__':
+#     main2()
 
 
-def main2():
-    ans = ''
-    datatoarray(data)
-    for i in range(197):
-        ip,name,status,ping = connection_check(i)
-        print(i)
-        if ip != 'None':
-            ans  = ans  + name + '--目前網路無法使用'+ '\n'
-    return ans
+# def test():
+#     headers = {
+#     'user-agent' : 'Mozilla/5.0'
+#     }
+#     r = requests.get("https://netflow.ntut.edu.tw/home/network/ping?encrypt_host=eyJpdiI6IjJyUGlVQ1NnRnQ3UkYwVEViak1uWVE9PSIsInZhbHVlIjoiaUdYemdnRGdKUDJnSkJLaU14anlQaGtxV0FOenFVaDc4YllPSVdBd0duST0iLCJtYWMiOiJlNDlkYjI5MzQ2ODEwNGNlYWIyNzg5Nzc4MGY0OGY4N2JmZjlmYzc1NDVlMGYwN2ZkNGE3NWY3ZjUyMWVlYjgxIn0=",  headers = headers)  
+#     r.encoding = r.apparent_encoding
+#     ans = '192.192.7.221'+ '臺大區網中心'
+#     return ans
 
+# def main2():
+#     ans = ''
+#     datatoarray(data)
+#     for i in range(197):
+#         ip,name,status,ping = connection_check(i)
+#         print(i)
+#         if ip != 'None':
+#             ans  = ans  + name + '--目前網路無法使用'+ '\n'
+#     print(ans)
 
-if __name__ == '__main__':
-    main()
-
-
+# def main(i):
+#     datatoarray(data)
+#     global localhost
+#     ip,name,status,ping = connection_check(i)
+#     #if ip != 'None':
+#     return ip,name,status,ping
